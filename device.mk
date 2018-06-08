9c88db6f2799a3e58917dded668b5d5c01ab90cc
Bruno Martins <bgcngm@gmail.com>
2018-06-08 12:14:54 +0100

    umi: overlays: Set up lights
    Change-Id: I6e84ac859a353612a00ae83557eaea2f077fa459

--- device.mk
+++ device.mk
@@ -89,7 +89,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # AID/fs configs
 PRODUCT_PACKAGES += \
