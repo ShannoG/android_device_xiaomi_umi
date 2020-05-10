7e49de134389b78c0fe573e4a8eab3f6ba4d6d28
MArshadR <mohamedarshadr@gmail.com>
2020-05-10 21:50:14 +0800

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,11 @@
+PRODUCT_MAKEFILES := \
+      aosp_umi.mk \
+      aosp_cmi.mk
+
+COMMON_LUNCH_CHOICES := \
+      aosp_umi-eng \
+      aosp_cmi-eng \
+      aosp_umi-user \
+      aosp_cmi-user \
+      aosp_umi-userdebug \ 
+      aosp_cmi-userdebug
