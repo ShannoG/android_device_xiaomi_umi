85b3024c9f068ee743ca1cda043a66994c0c2e76
MArshadR <mohamedarshadr@gmail.com>
2020-05-11 05:47:01 +0800

    Initial makefile

---
+++ havoc_umi.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from target device
+$(call inherit-product, device/xiaomi/umi/device.mk)
+
+# Inherit some Havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := arm64
+
+# Bootanimation res
+TARGET_BOOT_ANIMATION_RES := 720
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := havoc_umi
+PRODUCT_DEVICE := umi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 10
+PRODUCT_MANUFACTURER := Xiaomi
+
