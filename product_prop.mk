74bf7a0f51c2b89515b232b7fa381009a1bb10f7
QuantumShqipe <dr.techlc@gmail.com>
2020-05-16 06:49:57 +0800

    Imported display props from fw dump

--- product_prop.mk
+++ product_prop.mk
@@ -6,3 +6,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.secure=0 \
     ro.adb.secure=0
 endif
+
+# Display
+ro.surface_flinger.has_wide_color_display=true
+ro.surface_flinger.has_HDR_display=true
+ro.surface_flinger.use_color_management=true
+ro.surface_flinger.wcg_composition_dataspace=143261696
+ro.surface_flinger.protected_contents=true
