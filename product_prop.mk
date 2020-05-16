0746492a3e0c95d20b56035d433fdf9431eba04e
Akhil Narang <akhilnarang.1999@gmail.com>
2020-05-16 13:41:48 +0800

    Added more audio and fingerprint stuffs

--- product_prop.mk
+++ product_prop.mk
@@ -8,8 +8,28 @@ PRODUCT_PRODUCT_PROPERTIES += \
 endif
 
 # Display
-ro.surface_flinger.has_wide_color_display=true
-ro.surface_flinger.has_HDR_display=true
-ro.surface_flinger.use_color_management=true
-ro.surface_flinger.wcg_composition_dataspace=143261696
+ro.surface_flinger.has_wide_color_display=true \
+ro.surface_flinger.has_HDR_display=true \
+ro.surface_flinger.use_color_management=true \
+ro.surface_flinger.wcg_composition_dataspace=143261696 \
 ro.surface_flinger.protected_contents=true
+
+# Audio
+vendor.audio.feature.a2dp_offload.enable=true \
+vendor.audio.feature.anc_headset.enable=false \
+vendor.audio.feature.hifi_audio.enable=true \
+ro.vendor.audio.game.effect=true \
+ro.vendor.audio.scenario.support=true \
+ro.vendor.audio.sdk.fluencetype=fluence \
+ro.vendor.audio.sdk.ssr=false \
+ro.vendor.audio.sfx.earadj=true \
+ro.vendor.audio.sos=true \
+ro.vendor.audio.soundfx.type=mi \
+ro.vendor.audio.soundfx.usb=true \
+ro.vendor.audio.surround.support=true \
+ro.vendor.audio.spk.stereo=true
+
+# Fingerprint
+ro.hardware.fp.fod=true
+persist.vendor.sys.fp.fod.location.X_Y=441,1808 \
+persist.vendor.sys.fp.fod.size.width_height=197,197
