38fce96a16742daeae53e944ee653f0348107c6f
makorn645 <>
2020-08-07 12:39:07 +0100

    umi: Build Lineage Touch HAL

--- device.mk
+++ device.mk
@@ -53,6 +53,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml \
     $(LOCAL_PATH)/audio/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi_umi
 
 # GSI AVB Public Keys
 PRODUCT_PACKAGES += \
